b33eb67ffb6f938c1b5bedcf65f3fae7b5d6a08a
Fabiano Freitas <fabiano.freitas@gmail.com>
2023-12-14 13:07:00 -0300

    criacao de categoria

--- livro/admin.py
+++ livro/admin.py
@@ -1,6 +1,8 @@
 from django.contrib import admin
-from .models import Livros
+from .models import Livros, Categoria
 
 
+admin.site.register(Categoria)
 admin.site.register(Livros)
+
 
--- usuarios/models.py
+++ usuarios/models.py
@@ -5,6 +5,7 @@ class Usuario(models.Model):
     nome = models.CharField(max_length=30)
     email = models.EmailField()
     senha = models.CharField(max_length=64)
+    ativo = models.BooleanField(default=False)
 
     def __str__(self):
         return self.nome
